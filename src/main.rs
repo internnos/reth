0b166be538e5dabe311bbf58a8434028d5242973
internos <amajidsinar@gmail.com>
2022-10-28 14:05:24 +0700

    feat: derive debug

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+#[derive(Debug)]
 struct Account {
     address: String,
 }
 
+
+impl Account {
+    fn new(address: String) -> Self {
+        Account {
+            address
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Tx {
     from: Account,
     to: Account,
@@ -9,6 +20,32 @@ struct Tx {
     data: String,
 }
 
+
+impl Tx {
+    fn new(from: Account, to: Account, value: u64, data: String) -> Self {
+        Tx {
+            from,
+            to,
+            value,
+            data
+        }
+    }
+    fn reward(self) -> Self {
+        Tx {
+            data: String::from("reward"),
+            ..self
+        }
+    }
+} 
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let sender = Account::new(String::from("0x158"));
+    let receiver = Account::new(String::from("0x522"));
+
+    let transaction_bundle = Tx::new(sender, receiver, 1000, String::from("")).reward();
+    println!("{:?}", transaction_bundle);
+    // let tx = Tx::new()
 }
